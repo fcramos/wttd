b4cec13946a6212a0783ed39ef26a13ccf7ead79
Felipe Corrêa Ramos <lype086@gmail.com>
2014-02-15 23:24:58 -0200

    Testes unitários da Home Page

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -1,3 +1,19 @@
+# coding: utf-8
 from django.test import TestCase
 
-# Create your tests here.
+
+class HomeTest(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/')
+
+    def test_get(self):
+        """
+        GET / should return status 200
+        """
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_template(self):
+        """
+        Should get index.html template
+        """
+        self.assertTemplateUsed(self.response, 'index.html')
