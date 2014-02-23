c2b7b74cddefe8eb49246080bd1538405f080cbd
Felipe Corrêa Ramos <lype086@gmail.com>
2014-02-22 23:52:22 -0300

    Instalação do Django Admin Responsivo

--- eventex/settings.py
+++ eventex/settings.py
@@ -28,6 +28,7 @@ ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = (
+    'bootstrap_admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
